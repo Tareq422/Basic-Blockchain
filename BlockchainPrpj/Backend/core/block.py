b99cb60969753c3a19cd00b047973aa79ec544ed
ZiyadHusain <96144709+ZiyadHusain@users.noreply.github.com>
2023-12-07 15:20:53 +0300

    Delete BlockchainPrpj/Backend directory
    Replacing old backend with modified (Vids 1-7)

--- BlockchainPrpj/Backend/core/block.py
+++
@@ -1,8 +0,0 @@
-# BlockchainPrpj/Backend/core/block.py
-class Block:
-    def __init__(self, Height, Blocksize, BlockHeader, TxCount, Txs):
-        self.Height = Height
-        self.Blocksize = Blocksize
-        self.BlockHeader = BlockHeader
-        self.TxCount = TxCount
-        self.Txs = Txs
